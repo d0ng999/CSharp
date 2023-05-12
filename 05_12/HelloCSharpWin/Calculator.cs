fe5efe097faa200af8b43eb8bb9af37157452ec0
DonghyunHong <tlxmscksals@naver.com>
2023-05-12 23:00:34 +0900

    05.12

---
+++ 05_12/HelloCSharpWin/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloCSharpWin
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloLabel_Click(object sender, EventArgs e)
+        {
+            int num1 = 1;
+            int num2 = 2;
+
+            int sum = Add(num1, num2);
+
+            HelloLabel.Text = sum.ToString();
+        }
+         
+        private void SumNumbers_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(Sum1.Text);
+            int number2 = Convert.ToInt32(Sum2.Text);
+
+            int sum = Add(number1, number2);
+
+            SumResult.Text = sum.ToString();
+        }
+
+        public int Add(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            return sum;
+        }
+
+        public float Add(float number1, float number2)
+        { 
+            float sum = number1 + number2;
+            return sum;
+        }
+
+        public int Sub(int number1, int number2)
+        {
+            int sub = number1 - number2;
+            return sub;
+        }
+    }
+}
