b15955c2ec5b20107d202688f70391942df67bdf
DonghyunHong <tlxmscksals@naver.com>
2023-06-05 23:39:14 +0900

    06.05

---
+++ 06_05/BabyCarrot/Tools/Application.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyCarrot.Tools
+{
+    public static class Application
+    {
+        public static string Root
+        {
+            get
+            {
+                // 루트 폴더로 리턴해주는 구문
+                return AppDomain.CurrentDomain.BaseDirectory;
+            }
+        }
+    }
+}
